e9882127c12608c43d36c274fac76bb47784db79
Niko Matsakis <niko@alum.mit.edu>
2021-08-29 07:09:40 -0400

    automated test harness running all the steps

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod fact_parser;
 
-use std::path::Path;
+use std::{path::Path, process::Command};
 
 pub use fact_parser::generate_facts;
 
@@ -9,7 +9,32 @@ pub fn test_harness(dir_name: &str) -> eyre::Result<()> {
     let input_path = path.join("program.txt");
     let facts_path = path.join("facts");
     let data = std::fs::read_to_string(input_path)?;
+
     std::fs::create_dir_all(&facts_path)?;
     generate_facts(&data, &facts_path)?;
+
+    let output_path = path.join("output");
+    std::fs::create_dir_all(&output_path)?;
+
+    let _ = Command::new("souffle")
+        .args(&[
+            "src/polonius.dl".to_string(),
+            "-F".to_string(),
+            facts_path.display().to_string(),
+            "-D".to_string(),
+            output_path.display().to_string(),
+        ])
+        .output()?;
+
+    let dot = Command::new("python3")
+        .args(&[
+            "graphviz/graphviz.py".to_string(),
+            facts_path.display().to_string(),
+            output_path.display().to_string(),
+        ])
+        .output()?;
+
+    std::fs::write(output_path.join("graph.dot"), dot.stdout)?;
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-use polonius::generate_facts;
-use std::path::Path;
-
 fn main() -> eyre::Result<()> {
     for arg in std::env::args().skip(1) {
-        // Treat arg as a directory name:
-        let path = Path::new(&arg);
-        let input_path = path.join("program.txt");
-        let facts_path = path.join("facts");
-        let data = std::fs::read_to_string(input_path)?;
-        std::fs::create_dir_all(&facts_path)?;
-        generate_facts(&data, &facts_path)?;
+        polonius::test_harness(&arg)?;
     }
     Ok(())
 }
