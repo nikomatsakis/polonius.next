fd7d2e629ea3b38a68eb7ceef92ae06b37d03ce2
Rémy Rakic <remy.rakic@gmail.com>
2021-10-09 01:14:48 +0200

    start tracking `issue_47680` program facts

--- src/fact_emitter/test.rs
+++ src/fact_emitter/test.rs
@@ -196,3 +196,74 @@ fn example_vec_temp() {
     }
     "###);
 }
+
+#[test]
+fn example_issue_47680() {
+    let program = "
+        let temp: &'temp mut Thing;
+        let t0: &'t0 mut Thing;
+        let v: &'v mut Thing;
+
+        bb0: {
+            temp = &'L_Thing mut Thing;
+            goto bb1;
+        }
+
+        bb1: {
+            t0 = &'L_*temp mut *temp;
+            v = MaybeNext(move t0);
+            goto bb2, bb3;
+        }
+
+        bb2: {
+            temp = move v;
+            goto bb4;
+        }
+
+        bb3: {
+            goto bb4;
+        }
+
+        bb4: {
+            goto bb1;
+        }
+    ";
+    assert_debug_snapshot!(expect_facts(program), @r###"
+    Facts {
+        access_origin: [],
+        cfg_edge: [
+            (
+                "bb0[0]",
+                "bb1[0]",
+            ),
+            (
+                "bb1[0]",
+                "bb1[1]",
+            ),
+            (
+                "bb1[1]",
+                "bb2[0]",
+            ),
+            (
+                "bb1[1]",
+                "bb3[0]",
+            ),
+            (
+                "bb2[0]",
+                "bb4[0]",
+            ),
+            (
+                "bb3[0]",
+                "bb4[0]",
+            ),
+            (
+                "bb4[0]",
+                "bb1[0]",
+            ),
+        ],
+        clear_origin: [],
+        introduce_subset: [],
+        invalidate_origin: [],
+    }
+    "###);
+}
