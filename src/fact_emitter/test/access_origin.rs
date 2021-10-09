69d3ba415f28b7c373a4ecca0b9db8813871a139
Rémy Rakic <remy.rakic@gmail.com>
2021-10-09 15:44:16 +0200

    add access_origin test for simple assignments

--- src/fact_emitter/test/access_origin.rs
+++ src/fact_emitter/test/access_origin.rs
@@ -2,15 +2,39 @@ use super::*;
 use insta::assert_debug_snapshot;
 
 #[test]
+fn assignments_read_ref_arguments() {
+    let facts = expect_facts(
+        "
+        let a: &'a i32;
+        let ref: &'ref i32;
+
+        bb0: {
+            a = copy ref;
+        }
+    ",
+    );
+    assert_debug_snapshot!(facts.access_origin, @r###"
+    [
+        (
+            "'ref",
+            "bb0[0]",
+        ),
+    ]
+    "###);
+}
+
+#[test]
 fn function_calls_read_ref_arguments() {
-    let facts = expect_facts("
+    let facts = expect_facts(
+        "
         let i: i32;
         let ref: &'ref i32;
 
         bb0: {
             i = call(move ref);
         }
-    ");
+    ",
+    );
     assert_debug_snapshot!(facts.access_origin, @r###"
     [
         (
