91d068056c8ef5426d5f96577d0b0bcb60f1d130
Rémy Rakic <remy.rakic@gmail.com>
2021-10-09 14:56:13 +0200

    add test that shared borrows clear their origin

--- src/fact_emitter/test/clear_origin.rs
+++ src/fact_emitter/test/clear_origin.rs
@@ -26,3 +26,25 @@ fn assignments_to_references() {
 // fn all_origins_in_type_are_cleared_on_assignments() {
 //     // TODO
 // }
+
+#[test]
+fn shared_borrows_clear_their_origin() {
+    let facts = expect_facts(
+        "
+        let i: i32;
+        let ref: &'ref i32;
+
+        bb0: {
+            i = 0;
+            ref = &'L_i i;
+        }
+    ",
+    );
+    assert_eq!(
+        facts
+            .clear_origin
+            .into_iter()
+            .find(|(origin, _)| origin.0 == "'L_i"),
+        Some(("'L_i".into(), "bb0[1]".into()))
+    );
+}
