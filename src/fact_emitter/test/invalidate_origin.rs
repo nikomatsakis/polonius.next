7760d1a655f598a32b1a5f60ae32553792a42abb
Rémy Rakic <remy.rakic@gmail.com>
2021-10-10 13:07:54 +0200

    add actual loans in invalidate_origin tests

--- src/fact_emitter/test/invalidate_origin.rs
+++ src/fact_emitter/test/invalidate_origin.rs
@@ -6,19 +6,37 @@ fn assignment_to_non_references() {
     // Simple assignment
     let program = "
         let x: i32;
+        let y: &'y i32;
 
         bb0: {
             x = 22;
+            y = &'L_x x;
         }
     ";
-    assert_debug_snapshot!(expect_facts(program).invalidate_origin, @"[]");
+    assert_debug_snapshot!(expect_facts(program).invalidate_origin, @r###"
+    [
+        (
+            "'L_x",
+            "bb0[0]",
+        ),
+    ]
+    "###);
 
     // Function call return value
     let program = "
         let v: Vec;
+        let ref: &'ref Vec;
         bb0: {
             v = Vec_new();
+            ref = &'L_v v;
         }
     ";
-    assert_debug_snapshot!(expect_facts(program).invalidate_origin, @"[]");
+    assert_debug_snapshot!(expect_facts(program).invalidate_origin, @r###"
+    [
+        (
+            "'L_v",
+            "bb0[0]",
+        ),
+    ]
+    "###);
 }
