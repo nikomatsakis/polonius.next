3888d139362e1987b968be81323a6785ee93595e
Domenic Quirl <DomenicQuirl@pm.me>
2021-10-16 11:47:40 +0200

    get fact generation straight for manual tests

--- tests/examples.rs
+++ tests/examples.rs
@@ -14,3 +14,38 @@ fn issue_47680() -> eyre::Result<()> {
 fn vec_temp() -> eyre::Result<()> {
     polonius::test_harness("tests/vec-temp")
 }
+
+#[test]
+fn canonical_liveness() -> eyre::Result<()> {
+    polonius::test_harness("tests/canonical-liveness")
+}
+
+#[test]
+fn canonical_liveness_err() -> eyre::Result<()> {
+    polonius::test_harness("tests/canonical-liveness-err")
+}
+
+#[test]
+fn killing_and_murder() -> eyre::Result<()> {
+    polonius::test_harness("tests/killing-and-murder")
+}
+
+#[test]
+fn killing_and_murder_err() -> eyre::Result<()> {
+    polonius::test_harness("tests/killing-and-murder-err")
+}
+
+#[test]
+fn self_invalidation_loop() -> eyre::Result<()> {
+    polonius::test_harness("tests/self-invalidation-loop")
+}
+
+#[test]
+fn self_invalidation_loop_shared() -> eyre::Result<()> {
+    polonius::test_harness("tests/self-invalidation-loop-shared")
+}
+
+#[test]
+fn diamond_ref_mod() -> eyre::Result<()> {
+    polonius::test_harness("tests/diamond-ref-mod")
+}
